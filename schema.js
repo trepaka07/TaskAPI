26486bff62e4b4c766eb2983e5b08777b87e51c5
Attila Trepák <trepakattila07@gmail.com>
2025-03-03 21:03:32 +0100

    login

--- schema.js
+++ schema.js
@@ -1,5 +1,9 @@
 const { z } = require("zod");
 
+const userSchema = z.object({
+  username: z.string().min(3).nullable(),
+});
+
 const signupSchema = z.object({
   username: z.string().min(3),
   email: z.string().email(),
@@ -7,7 +11,7 @@ const signupSchema = z.object({
 });
 
 const loginSchema = z.object({
-  email: z.string().email(),
+  username: z.string().min(3), // TODO: or email
   password: z.string().min(8), // TODO: contains number
 });
 
@@ -15,4 +19,4 @@ const taskSchema = z.object({
   name: z.string().min(3),
 });
 
-module.exports = { signupSchema, loginSchema, taskSchema };
+module.exports = { userSchema, signupSchema, loginSchema, taskSchema };
--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 const bcrypt = require("bcrypt");
-const { loginSchema, signupSchema, taskSchema } = require("./schema");
+const {
+  loginSchema,
+  signupSchema,
+  taskSchema,
+  userSchema,
+} = require("./schema");
 const PORT = 3005;
 
 require("dotenv").config();
@@ -20,22 +25,75 @@ const pool = mysql.createPool({
 const sendDefaultError = (res, message = "Something went wrong") =>
   res.status(500).send({ error: message });
 
-const validateUserBody = async (req, schema) => {
-  const body = await req.body;
-  const validation = schema.safeParse(body);
-  return validation.success;
-};
-
 const validateSignup = async (req, res, next) => {
-  const validate = await validateUserBody(req, signupSchema);
-  if (!validate) res.status(400).send(result.error);
-  else next();
+  const validate = signupSchema.safeParse(req.body);
+  if (!validate.success) {
+    console.log(validate.error);
+    res.status(400).send({ error: "Invalid input" });
+  } else next();
 };
 
 const validateLogin = async (req, res, next) => {
-  const validate = await validateUserBody(req, loginSchema);
-  if (!validate) res.status(400).send(result.error);
+  const validate = loginSchema.safeParse(req.body);
+  if (!validate.success) res.status(400).send({ error: "Invalid input" });
   else next();
+};
+
+// TODO: use this
+const getConnection = async () =>
+  new Promise((resolve, reject) => {
+    pool.getConnection((err, con) => {
+      if (err || !con) reject(err);
+      else resolve(con); // TODO: check affected rows count
+    });
+  });
+
+// TODO: merge
+const userExists = async (req, res, next) => {
+  const validate = userSchema.safeParse(req.body);
+  if (!validate.success) {
+    console.log(validate.error);
+    res.status(400).send({ error: "Invalid input" });
+    return;
+  }
+
+  pool.getConnection((err, con) => {
+    if (err || !con) sendDefaultError(res);
+    else {
+      con.query(
+        `SELECT * FROM users WHERE username = '${req.body.username}';`,
+        (err, result) => {
+          if (err) sendDefaultError(res);
+          else if (result && result.length > 0) next();
+          else res.status(404).send({ error: "User not found" });
+        }
+      );
+    }
+  });
+};
+
+const userNotExists = async (req, res, next) => {
+  const validate = userSchema.safeParse(req.body);
+  if (!validate.success) {
+    console.log(validate.error);
+    res.status(400).send({ error: "Invalid input" });
+    return;
+  }
+
+  pool.getConnection((err, con) => {
+    if (err || !con) sendDefaultError(res);
+    else {
+      con.query(
+        `SELECT * FROM users WHERE username = '${req.body.username}';`,
+        (err, result) => {
+          if (err) sendDefaultError(res);
+          else if (result && result.length > 0)
+            res.status(409).send({ error: "User already exists" });
+          else next();
+        }
+      );
+    }
+  });
 };
 
 app.use("/users", userExists);
@@ -54,19 +112,36 @@ app.get("/tasks", (req, res) => {
   });
 });
 
-app.post("/signup", validateSignup, async (req, res) => {
+app.post("/signup", [validateSignup, userNotExists], async (req, res) => {
   const body = await req.body;
 
   pool.getConnection((err, con) => {
     if (err || !con) sendDefaultError(res);
     else {
-      const passwordHash = bcrypt.hashSync(body.password, 10);
-
+      const hash = bcrypt.hashSync(password, 10);
       con.query(
-        `INSERT INTO users (username, email, password) VALUES ('${body.username}', '${body.email}', '${passwordHash}');`,
+        `INSERT INTO users (username, email, password) VALUES ('${body.username}', '${body.email}', '${hash}');`,
         (err, result) => {
           if (err) sendDefaultError(res, err); // TODO: more accurate message
           else res.status(201).send(result);
+        }
+      );
+    }
+  });
+});
+
+app.post("/login", [validateLogin, userExists], (req, res) => {
+  pool.getConnection((err, con) => {
+    if (err || !con) sendDefaultError(res);
+    else {
+      con.query(
+        `SELECT * FROM users WHERE username = '${req.body.username}';`, // TODO: get the user from the middleware
+        (err, result) => {
+          if (!err && result && result.length > 0) {
+            if (bcrypt.compareSync(req.body.password, result[0].password))
+              res.send({ data: "Access granted" });
+            else res.status(401).send({ error: "Invalid password" });
+          } else sendDefaultError(res, err); // TODO: more accurate message
         }
       );
     }
